a51f05f61f44e38895a2e10e648f349a46a5683e
SAIF ALI <113752122+saifali509@users.noreply.github.com>
2026-02-15 13:19:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your JS code here. If required.
+const form = document.querySelector("#loginForm");
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+const checkbox = document.querySelector("#checkbox");
+const existingBtn = document.querySelector("#existing");
+
+window.addEventListener("DOMContentLoaded", function () {
+  const savedUsername = localStorage.getItem("username");
+  const savedPassword = localStorage.getItem("password");
+
+  if (savedUsername && savedPassword) {
+    existingBtn.style.display = "inline-block";
+  }
+});
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  alert(`Logged in as ${username}`);
+
+  if (checkbox.checked) {
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+    existingBtn.style.display = "inline-block";
+  } else {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+    existingBtn.style.display = "none";
+  }
+
+  form.reset();
+});
+existingBtn.addEventListener("click", function () {
+  const savedUsername = localStorage.getItem("username");
+
+  if (savedUsername) {
+    alert(`Logged in as ${savedUsername}`);
+  }
+});
